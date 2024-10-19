c7cdd82eea2232a312eda9b366b287b747ff0e7f
mortza mansory <79837660+mortzaCFT@users.noreply.github.com>
2024-10-19 15:33:57 +0330

    Update TextCard.dart

--- lib/screens/components/TextCard.dart
+++ lib/screens/components/TextCard.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:notepad/data/data.dart';
@@ -19,7 +21,7 @@ class TextCard extends StatelessWidget {
     return Container(
       width: MediaQuery.of(context).size.width - 40,
       padding: const EdgeInsets.all(15),
-      margin: const EdgeInsets.symmetric(vertical: 5), 
+      margin: const EdgeInsets.symmetric(vertical: 5),
       decoration: BoxDecoration(
         color: note.color,
         borderRadius: BorderRadius.circular(10),
@@ -30,29 +32,37 @@ class TextCard extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  note.title,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
+                if (note.signaturePath != null) ...[
+                  Image.file(
+                    File(note.signaturePath!),
+                    width: 100,
+                    height: 50,
                   ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  note.text,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.black,
+                ] else ...[
+                  Text(
+                    note.title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
+                  SizedBox(height: 5),
+                  Text(
+                    note.text,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
                 SizedBox(height: 10),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Text(
-                      formattedDate, 
+                      formattedDate,
                       style: TextStyle(
                         fontSize: 12,
                         fontWeight: FontWeight.w300,
