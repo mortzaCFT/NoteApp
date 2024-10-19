1e4a3d0d88df235857e0752ef15539ad833e6440
mortza mansory <79837660+mortzaCFT@users.noreply.github.com>
2024-10-19 15:31:52 +0330

    Update data.dart

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Data {
+  String id;  
   String title;
   String text;
   Color color;
   DateTime dateCreated; 
+  String? signaturePath; 
 
   Data({
+    required this.id,  
     required this.title,
     required this.text,
     required this.color,
-    required this.dateCreated, 
+    required this.dateCreated,
+    this.signaturePath,
   });
 }
